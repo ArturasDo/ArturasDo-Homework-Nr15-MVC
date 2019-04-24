2a74c2f7c911754a1b244fb7b7ebeb9df98db46b
ArturasDo <48279198+ArturasDo@users.noreply.github.com>
2019-04-24 18:22:33 +0300

    paleidimas MainHair.py

---
+++ mainHair.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("HairSalon.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("Fakebook.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("Layouts.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
